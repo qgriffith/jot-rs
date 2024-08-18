93d27dc9789739e2473cb181dbf6d5c055793489
qgriffith <qgriffith@gmail.com>
2024-08-18 15:55:02 -0400

    Enhance prompt styling with `owo-colors`
    Added `owo-colors` for styling prompt messages in the terminal. Now, prompts display with bold and colored text, improving readability and user experience. Updated Cargo.toml and Cargo.lock to include the new dependency.

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,13 @@
-use std::{fs, io, io::Write, path::PathBuf};
 use edit::{edit_file, Builder};
+use owo_colors::OwoColorize;
+use std::{fs, io, io::Write, path::PathBuf};
 
 fn ask_for_filename() -> io::Result<String> {
     rprompt::prompt_reply(
         "Enter filename\
-        > ",
+        > "
+        .blue()
+        .bold(),
     )
 }
 
@@ -14,8 +17,9 @@ fn confirm_filename(raw_title: &str) -> io::Result<String> {
         // this is a convention not a req
         let result = rprompt::prompt_reply(&format!(
             "current title: {}
-Do you want a different title? (y/N): ",
-            &raw_title,
+Do you want a different title? (y/{}): ",
+            &raw_title.bold().green(),
+            "N".bold()
         ))?;
 
         match result.as_str() {
@@ -30,36 +34,30 @@ Do you want a different title? (y/N): ",
     }
 }
 
-pub fn write(
-    jot_path: PathBuf,
-    title: Option<String>,
-) -> Result<(), std::io::Error> {
+pub fn write(jot_path: PathBuf, title: Option<String>) -> Result<(), std::io::Error> {
     let (mut file, filepath) = Builder::new()
         .suffix(".md")
         .rand_bytes(5)
         .tempfile_in(&jot_path)?
         .keep()?;
     dbg!(&filepath);
-    let template =
-        format!("# {}", title.as_deref().unwrap_or(""));
+    let template = format!("# {}", title.as_deref().unwrap_or(""));
     file.write_all(template.as_bytes())?;
     edit_file(&filepath)?;
     let contents = fs::read_to_string(&filepath)?;
 
     let document_title = title.or_else(|| {
-        contents.lines().find(|v| v.starts_with("# ")).map(
-            |line| {
-                line.trim_start_matches("# ").to_string()
-            },
-        )
+        contents
+            .lines()
+            .find(|v| v.starts_with("# "))
+            .map(|line| line.trim_start_matches("# ").to_string())
     });
 
     let filename = match document_title {
         Some(raw_title) => confirm_filename(&raw_title),
         None => ask_for_filename(),
     }
-        .map(|title| slug::slugify(title))?;
-
+    .map(|title| slug::slugify(title))?;
 
     for attempt in 0.. {
         let mut dest = jot_path.join(if attempt == 0 {
